b81b9c791aa09907a8066590295f3692d36083ff
FadjiaLeila <fadjia20@gmail.com>
2019-07-28 02:17:19 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -0,0 +1,103 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class TimelineActivity extends AppCompatActivity {
+private TwitterClient client;
+private RecyclerView rvTweets;
+private TweetsAdapter adapter;
+private List<Tweet> tweets;
+private SwipeRefreshLayout swipeContainer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+      //  getSupportActionBar().setDisplayShowHomeEnabled(true);
+       // getSupportActionBar().setLogo(R.drawable.logo);
+       // getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        client = TwitterApp.getRestClient(this);
+        swipeContainer= findViewById(R.id.swipeContainer);
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        // find the recyclerview
+        rvTweets = findViewById(R.id.rvTweets);
+        tweets= new ArrayList<>();
+        adapter= new TweetsAdapter(this, tweets);
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(adapter);
+        populateHomeTimeline();
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.d("TwitterClient", "Content is being refresh");
+                populateHomeTimeline();
+            }
+        });
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+               // Toast.makeText(TimelineActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                // Iterate through the list of tweets
+                List<Tweet> tweetsToAdd = new ArrayList<>();
+                for (int i =0; i<response.length(); i++) {
+                    try {
+                        // convert a jsonobject into a tweet object
+                      JSONObject jsonTweetObject =  response.getJSONObject(i);
+                      Tweet tweet = Tweet.fromJson(jsonTweetObject);
+                      // add tweet into the data source
+                        tweetsToAdd.add(tweet);
+                     //   tweets.add(tweet);
+                        // notify the adapter
+                      //  adapter.notifyItemInserted(tweets.size()-1);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                adapter.clear();
+                adapter.addTweets(tweetsToAdd);
+                swipeContainer.setRefreshing(false);
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.e("TwitterClient", responseString);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.e("TwitterClient", errorResponse.toString());
+            }
+        });
+    }
+}
